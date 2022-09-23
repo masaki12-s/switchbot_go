ae3e8ecacd45d626a4bda409df03fad371c47236
masaki12-s <asam.archer1112@yahoo.ne.jp>
2022-09-24 02:33:21 +0900

    .envに対応

--- API/getdevice.go
+++ API/getdevice.go
@@ -1,6 +1,33 @@
 package main
-import "fmt"
-
+import (
+	"fmt"
+	// "encoding/json"
+	// "net/http"
+	"os"
+	"github.com/joho/godotenv"
+)
 func main(){
-	fmt.Println("aaa")
+	token, secret := loadEnv()
+	fmt.Println(token,secret)
+}
+
+func sign(string, string) (string, string, string){
+	return "a","b","c"
+}
+
+func loadEnv() (string, string){
+	// ここで.envファイル全体を読み込みます。
+	// この読み込み処理がないと、個々の環境変数が取得出来ません。
+	// 読み込めなかったら err にエラーが入ります。
+	err := godotenv.Load(".env")
+	
+	// もし err がnilではないなら、"読み込み出来ませんでした"が出力されます。
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	} 
+	
+	// .envの SAMPLE_MESSAGEを取得して、messageに代入します。
+	token := os.Getenv("token")
+	secret := os.Getenv("secret")
+	return token, secret
 }
